6e7c6b8c60ecd83fa84af567ce969f12453aa713
Sadi rahman <42184120+sadirahman@users.noreply.github.com>
2019-05-26 02:28:43 +0600

    New

--- Data_analysis_example.py
+++ Data_analysis_example.py
@@ -1,25 +1,35 @@
 import pandas as pd
+from pandas import ExcelWriter
 
 def Number_of_line(cell):
     if cell =="None":
         return 'NaN'
     return cell
 
-df = pd.read_excel("C:\\Users\\Sun of sadi\\PycharmProjects\\Pandas\Data\\Demo.xlsx", "Sheet1", converters={
+df = pd.read_excel("C:\\Users\\Sun of sadi\\PycharmProjects\\Pandas\Data\\book1.xlsx", "Sheet1", converters={
       'Number  of line': Number_of_line
 })
 print(df)
-df.to_excel('New_Data.xlsx',sheet_name='Demo')
+df.to_excel('New_Data.xlsx',sheet_name='book1')
 print(df)
 
 print("show Our result:")
 
 
 Group = df.groupby('Name')
+w = ExcelWriter('nam.xlsx')
+c = ExcelWriter('count.xlsx')
 for Name,name_df in Group:
     print(Name)
     print(name_df)
-    print(name_df.count())
+    Count = name_df.count()
+    print(Count)
+    Count.to_excel(c, sheet_name='sheet ' + str(Name))
+    c.save()
+    name_df.to_excel(w, sheet_name='sheet ' + str(Name))
+    w.save()
+
+
 
 
 
